5eac5cb5ea6ccc690257c90496d47fbfc777c851
Chanira <chanirapannala@gmail.com>
2024-04-19 17:24:03 +0530

    feat: drag and drop

---
+++ higherOrderfunc.js
@@ -0,0 +1,45 @@
+const companies = [
+  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i].name);
+// }
+
+// companies.forEach((company) => console.log(company.name));
+
+// filter
+// Get 21 and older
+// let canDrink = [];
+// for (let index = 0; index < ages.length; index++) {
+//   if (ages[index] >= 21) {
+//     canDrink.push(ages[index]);
+//   }
+// }
+
+let canDrink = ages.filter((age) => age >= 21);
+let retailCompanies = companies.filter(
+  (company) => company.category == "Retail"
+);
+
+// console.log(retailCompanies);
+let companyNames = [];
+companies.forEach(function (company) {
+  companyNames.push(company.name);
+});
+
+console.log(
+  companies.map(
+    (company) => `${company.name} [${company.start} - ${company.end}]`
+  )
+);
